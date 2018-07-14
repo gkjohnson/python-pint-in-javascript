7216863e374b16c84ded14bcf6e77ed3c3aed743
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-07-13 17:38:31 -0700

    Move code

--- pint.js/pint.js
+++ pint.js/pint.js
@@ -1,9 +1,15 @@
-(async () => {
+const pypy = 
+    new pypyjs({
+        stdin: pypyjs._defaultStdin,
+        stdout: pypyjs._defaultStdout,
+        stderr: pypyjs._defaultStderr
+    });
 
-    function writeFile(name, dir, content) {
+window.pypy = pypy;
 
-        console.log(`adding '${ name }' in '${ dir }'`);
-        const script = 
+function writeFile(name, dir, content) {
+        
+    const script = 
 `
 import os
 LIB_PATH = "${ dir }"
@@ -15,28 +21,28 @@ with open(dir, "w") as f:
     f.write(script)
 
 `;
-
-        return pypyjs.exec(script);
         
-    }
+    return pypy.exec(script);
+                
+}
 
-    function defineModule(name, content) {
+function defineModule(name, content) {
+    
+    return writeFile(name, '/lib/pypyjs/lib_pypy/', content);
+    
+}
 
-        return writeFile(name, '/lib/pypyjs/lib_pypy/', content);
+(async () => {
 
-    }
+    await pypy.ready();
 
-    console.log('Waiting for pypyjs to be ready...');
-    await pypyjs.ready();
-    console.log('Ready...');
-
-    pypyjs.exec(
+    pypy.exec(
 `
 import os
 os.makedirs("/lib/pypyjs/lib_pypy/pint")
 os.makedirs("/lib/pypyjs/lib_pypy/pint/compat")
 `);
-    
+
     const modules = await fetch('./modules.json').then(res => res.json());
     const promises = [];
     Object
@@ -62,12 +68,26 @@ os.makedirs("/lib/pypyjs/lib_pypy/pint/compat")
 
     await Promise.all(promises);
 
-    pypyjs.exec(
+    pypy.exec(
 `
 import pint.compat
 ureg = pint.UnitRegistry('default_en.txt')
+Q = ureg.Quantity
+
 print 3 * ureg.meter + 4 * ureg.cm
 `)
 
-
 })();
+
+// export
+// class PintRegistry {
+
+//     get ready() { return pypy.isReady(); }
+    
+//     covert(val, from, to) {
+
+//         window.
+
+//     }
+
+// }
